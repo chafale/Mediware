c42dc35186ce28bce4fc592944e8b57ed43690b9
Dhananjay Hedaoo <dhananjay_hedaoo1@persistent.co.in>
2018-01-12 14:26:23 +0530

    added weather UI

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,7 @@ import * as Chartist from 'chartist';
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
+  
 })
 export class HomeComponent implements OnInit {
     public emailChartType: ChartType;
